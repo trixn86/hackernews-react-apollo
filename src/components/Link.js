6209718e012e8f66b35992e68da4cb6aea844d9a
Benjamin Brauch <brauch@cosmocode.de>
2018-05-14 21:23:01 +0200

    exercise 6

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,19 +1,74 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+
+import {AUTH_TOKEN} from '../constants';
+import {timeDifferenceForDate} from '../utils';
+
 
 class Link extends Component {
     render() {
+        const authToken = localStorage.getItem(AUTH_TOKEN);
+        const {index, link} = this.props;
+
         return (
-            <div>
-                <div>
-                    {this.props.link.description} ({this.props.link.url})
+            <div className="flex mt2 items-start">
+                <div className="flex items-center">
+                    <span className="gray">{index + 1}.</span>
+                    {authToken && (
+                        <div className="ml1 gray f11" onClick={() => this._voteForLink()}>
+                            ▲
+                        </div>
+                    )}
+                </div>
+                <div className="ml1">
+                    <div>
+                        {link.description} ({link.url})
+                    </div>
+                    <div className="f6 lh-copy gray">
+                        {link.votes.length} votes | by{' '}
+                        {link.postedBy
+                            ? link.postedBy.name
+                            : 'Unknown'}{' '}
+                        {timeDifferenceForDate(link.createdAt)}
+                    </div>
                 </div>
             </div>
-        )
+        );
     }
 
     _voteForLink = async () => {
-        // ... you'll implement this in chapter 6
+        const {link: {id: linkId}, voteMutation, updateStoreAfterVote} = this.props;
+
+        await voteMutation({
+            variables: {
+                linkId,
+            },
+            update: (store, {data: {vote}}) => {
+                updateStoreAfterVote(store, vote, linkId)
+            }
+        })
     }
 }
 
-export default Link;
+const VOTE_MUTATION = gql`
+    mutation VoteMutation($linkId: ID!) {
+        vote(linkId: $linkId) {
+            id
+            link {
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+            user {
+                id
+            }
+        }
+    }
+`;
+
+
+export default graphql(VOTE_MUTATION, {name: 'voteMutation'})(Link);
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,6 +4,13 @@ import gql from 'graphql-tag';
 import {graphql} from 'react-apollo';
 
 class LinkList extends Component {
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+        const data = store.readQuery({ query: FEED_QUERY });
+        const votedLink = data.feed.links.find(link => link.id === linkId);
+        votedLink.votes = createVote.link.votes;
+        store.writeQuery({ query: FEED_QUERY, data });
+    };
+
     render() {
         const {feedQuery} = this.props;
 
@@ -18,12 +25,16 @@ class LinkList extends Component {
         const linksToRender = feedQuery.feed.links;
 
         return (
-            <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+            <div>
+                {linksToRender.map((link, index) => (
+                    <Link key={link.id} updateCacheAfterVote={this._updateCacheAfterVote} index={index} link={link} />
+                ))}
+            </div>
         );
     }
 }
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
     query FeedQuery {
         feed {
             links {
@@ -31,6 +42,16 @@ const FEED_QUERY = gql`
                 createdAt
                 url
                 description
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
             }
         }
     }
