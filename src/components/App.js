2811802f79cf2aed8f810cf4c9ce1c260503bca1
Benjamin Brauch <brauch@cosmocode.de>
2018-05-14 20:47:23 +0200

    exercise 5

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import LinkList from './LinkList';
+import {Route, Switch} from 'react-router-dom';
+
 import CreateLink from './CreateLink';
 import Header from './Header';
-import {Route, Switch} from 'react-router-dom';
+import LinkList from './LinkList';
+import Login from './Login';
+
 
 class App extends Component {
     render() {
@@ -13,8 +16,9 @@ class App extends Component {
                 <Header />
                 <div className="ph3 pv1 background-gray">
                     <Switch>
-                        <Route exact path="/" component={LinkList} />
+                        <Route exact path="/login" component={Login} />
                         <Route exact path="/create" component={CreateLink} />
+                        <Route exact path="/" component={LinkList} />
                     </Switch>
                 </div>
             </div>
