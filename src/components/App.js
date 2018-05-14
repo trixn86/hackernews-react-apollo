ae22a300acd9e8190b70db8ea66022b5d6fd58a2
Benjamin Brauch <brauch@cosmocode.de>
2018-05-14 21:36:18 +0200

    exercise 7

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
 import Login from './Login';
+import Search from './Search';
 
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
                         <Route exact path="/login" component={Login} />
                         <Route exact path="/create" component={CreateLink} />
                         <Route exact path="/" component={LinkList} />
+                        <Route exact path='/search' component={Search}/>
                     </Switch>
                 </div>
             </div>
